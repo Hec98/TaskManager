3e4222d196619682c7b18faeb2731f1623992ea6
Hec98 <hector982015@gmail.com>
2021-12-04 23:08:32 -0600

    Added interface

--- db.py
+++ db.py
@@ -1,9 +1,9 @@
-from tkinter import Tk
+from tkinter import Tk, Label, Entry, Button, LabelFrame
 import sqlite3
 
 root = Tk()
 root.title('Task Manager')
-root.wm_geometry('500x500')
+root.geometry('410x300')
 
 conn = sqlite3.connect('todo.db')
 c = conn.cursor()
@@ -18,3 +18,26 @@ c.execute("""
 """)
 
 conn.commit()
+
+def addTodo():
+    todo = e.get()
+    c.execute('INSERT INTO todo (description, completed) VALUES (?, ?)', (todo, False))
+    conn.commit()
+    e.delete(0, 'end')
+
+
+lb = Label(root, text='Task')
+lb.grid(row=0, column=0)
+
+e = Entry(root, width=40)
+e.focus()
+e.grid(row=0, column=1)
+
+btn = Button(root, text='Add', command=addTodo)
+btn.grid(row=0, column=2)
+
+frame = LabelFrame(root, text='My tasks', padx=5, pady=5)
+frame.grid(row=1, column=0, columnspan=3, sticky='nswe', padx=5)
+
+root.bind('<Return>', lambda _: addTodo())
+root.mainloop()
